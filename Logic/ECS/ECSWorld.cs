0950b4d1488baca13df0d7a5bdff932ef973ffe5
ph7-5672 <h7peng@163.com>
2025-11-16 22:55:44 +0800

    ECS框架完善

---
+++ Logic/ECS/ECSWorld.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using GodotMonoGeneral.Utils;
+
+namespace GodotMonoGeneral.Logic.ECS;
+
+public class ECSWorld
+{
+    private int nextEntityId = 0;
+    private readonly Queue<int> freeIds = new();
+    private readonly HashSet<int> activeIds = [];
+    private readonly HashSet<ISparseSet> sparses = [];
+    public int ActiveCount => activeIds.Count;
+    public int AvailableCount => freeIds.Count;
+
+    /// <summary>
+    /// 创建实体。
+    /// </summary>
+    /// <returns></returns>
+    public int CreateEntity()
+    {
+        if (freeIds.Count > 0)
+        {
+            int recycledId = freeIds.Dequeue();
+            activeIds.Add(recycledId);
+            return recycledId;
+        }
+        
+        int newId = nextEntityId++;
+        activeIds.Add(newId);
+        return newId;
+    }
+    
+    /// <summary>
+    /// 删除实体，回收id。
+    /// </summary>
+    /// <param name="id"></param>
+    public void DeleteEntity(int id)
+    {
+        if (activeIds.Remove(id))
+        {
+            freeIds.Enqueue(id);
+            foreach (var sparse in sparses)
+            {
+                sparse.Delete(id);
+            }
+        }
+    }
+
+    private SparseSet<T> RegisterComponent<T>() where T : struct
+    {
+        var sparseSet = SingletonFactory.GetSingleton<SparseSet<T>>();
+        sparses.Add(sparseSet);
+        return sparseSet;
+    }
+
+    /// <summary>
+    /// 判断实体是否拥有指定类型组件。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="entityId"></param>
+    /// <returns></returns>
+    public bool HasComponent<T>(int entityId) where T : struct
+    {
+        var sparseSet = RegisterComponent<T>();
+        return sparseSet.Has(entityId);
+    }
+
+    /// <summary>
+    /// 实体获取指定类型组件。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="entityId"></param>
+    /// <returns></returns>
+    public ref T GetComponent<T>(int entityId) where T : struct
+    {
+        var sparseSet = RegisterComponent<T>();
+        return ref sparseSet.Get(entityId);
+    }
+    
+    /// <summary>
+    /// 实体添加或修改组件。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="entityId"></param>
+    /// <param name="component"></param>
+    public void AddComponent<T>(int entityId, ref T component) where T : struct
+    {
+        var sparseSet = RegisterComponent<T>();
+        sparseSet.Add(entityId, ref component);
+    }
+
+    /// <summary>
+    /// 实体删除组件。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="entityId"></param>
+    public void DeleteComponent<T>(int entityId) where T : struct
+    {
+        var sparseSet = RegisterComponent<T>();
+        sparseSet.Delete(entityId);
+    }
+
+   
+}
